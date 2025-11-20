8b72f940e2ca40b229608c5a58e63d4178094362
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-20 16:29:03 -0300

    update js/app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function comprar(){
+//Recuperar Os Valores
+let quantidade = document.getElementById("qtd");
+let pista = 100; 
+let cadeiraSuperior = 200; 
+let cadeiraInferior = 400; 
+let textoPista = document.getElementById("qtd-pista");
+textoPista.innerHTML = `<li>Pista<span id="qtd-pista">${pista}</span></li>`;
+let textoCadeiraSuperior = document.getElementById("qtd-superior");
+let textoCadeiraInferior = document.getElementById("qtd-inferior");
+//adicionar botao de compra
+//Descontar ingressos do caixa
+//colocar limite de compra de ingresso
+}
