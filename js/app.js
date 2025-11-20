19c814923538a945df2f10dd8c7b8b76a2bf5d18
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-20 17:36:04 -0300

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,2 @@
-//adicionar botao de compra
 function comprar(){
-//Recuperar Os Valores
-let quantidade = document.getElementById("qtd").value;
-let pista = 100; 
-let superior = 200; 
-let inferior = 400; 
-let textoPista = document.getElementById("qtd-pista");
-textoPista.textContent = `${pista}`;
-let textoCadeiraSuperior = document.getElementById("qtd-superior");
-textoCadeiraSuperior.textContent = `${superior}`;
-let textoCadeiraInferior = document.getElementById("qtd-inferior");
-textoCadeiraInferior.textContent = `${inferior}`;
-//Descontar ingressos do caixa
-let ingressoEscolhido = document
-.getElementById("tipo-ingresso");
-let tipoIngresso = ingressoEscolhido.value;
-if(tipoIngresso == "superior"){
-  superior = superior - quantidade;
-}
-//colocar limite de compra de ingresso
 }
