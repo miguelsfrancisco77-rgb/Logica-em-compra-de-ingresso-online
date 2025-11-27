c1c9e6b6a87016a489dcbc7f63dce839e844aff8
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-27 18:22:47 -0300

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -4,9 +4,16 @@ let inferior = 400;
 let textoPista = document.getElementById('qtd-pista');
 let textoSuperior = document.getElementById('qtd-superior');
 let textoInferior = document.getElementById('qtd-inferior');
-textoPista.textContent = `${pista}`;
-textoSuperior.textContent = `${superior}`;
-textoInferior.textContent = `${inferior}`;
 function comprar(){
-  pista = pista - 10
+  let tipo = document.getElementById('tipo-ingresso');
+  let quantidade = document.getElementById('qtd');
+  if(tipo.value == "inferior" && quantidade =< inferior){
+    inferior = inferior - quantidade;
+  }
+  
+  
+  
+  textoPista.textContent = `${pista}`;
+  textoSuperior.textContent = `${superior}`;
+  textoInferior.textContent = `${inferior}`;
 }
