737e5a15071397480bf56c080f2ecee1617c974e
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-28 16:02:23 -0300

    update app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+let pista = 100;
+let superior = 200;
+let inferior = 400;
+let textoPista = document.getElementById('qtd-pista');
+let textoSuperior = document.getElementById('qtd-superior');
+let textoInferior = document.getElementById('qtd-inferior');
+function comprar(){
+  let tipo = document.getElementById('tipo-ingresso');
+  let quantidade = document.getElementById('qtd').value;
+  if(quantidade > pista){
+    alert("quantidade indisponivel");
+  } else(tipo.value == "pista" && quantidade <= pista){
+    pista = pista - quantidade;
+  }
+  if(tipo.value == "superior" && quantidade <= superior){
+    superior = superior - quantidade;
+  }
+  if(tipo.value == "inferior" && quantidade <= inferior){
+    inferior = inferior - quantidade;
+  }
+  textoPista.textContent = `${pista}`;
+  textoSuperior.textContent = `${superior}`;
+  textoInferior.textContent = `${inferior}`;
+}
